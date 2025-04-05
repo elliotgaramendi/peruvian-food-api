a6453488af85d76bf7630f0dd50482c80de67893
elliotgaramendi <elliotgaramendi@gmail.com>
2025-04-05 10:58:34 -0500

    feat: enhance server response and add food routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
+import cors from 'cors';
 import express from 'express';
+import foodRouter from './routes/foodRoutes.js';
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.json({ message: 'The server is running!' });
+  res.json({
+    message: "Welcome to the Peruvian Food API",
+    endpoints: {
+      getAllFoods: "/api/foods",
+      getFoodById: "/api/foods/1",
+      filterFoods: "/api/foods?search=chi",
+      combinedFilters: "/api/foods?search=gallina&category=main&isSpicy=true"
+    }
+  });
+});
+
+app.use('/api/foods', foodRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
 });
 
 app.listen(PORT, () => {
